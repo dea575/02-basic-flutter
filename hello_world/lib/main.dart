6aa56d068785c4580b9797c2ba520ccf6ddbb17d
dea575 <89899114+dea575@users.noreply.github.com>
2022-03-30 09:28:26 +0700

    Update main.dart

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -1,140 +1,186 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
-  MyApp({Key? key}) : super(key: key);
-
   @override
-  State<MyApp> createState() => _MyAppState();
+  _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  TextEditingController text1 = TextEditingController();
-  double kelvin = 0, reamor = 0, input = 0;
-
-  konversi() {
-    setState(() {
-      input = double.parse(text1.text);
-      kelvin = input + 273;
-      reamor = input * 4 / 5;
-    });
-  }
+  List<Widget> widgets = [];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Center(child: Text('Konversi Suhu')),
-        ),
-        body: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              inputSuhu(text1: text1),
-              Hasil(kelvin: kelvin, reamor: reamor),
-              konvertButton(text1: text1, konversi: konversi)
-            ],
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("MyApp"),
+      ),
+      body: Container(
+        child: ListView(children: [
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.only(left: 20, top: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  Text(
+                    "BERITA TERBARU",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    "PERTANDINGAN HARI INI",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
           ),
-        ),
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 10),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Image.network(
+                "https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2020/10/12/50343832.jpg",
+                width: 395,
+                height: 250,
+                fit: BoxFit.fill,
+              ),
+            ]),
+          ),
+          Container(
+            padding: const EdgeInsets.only(
+              left: 20,
+              top: 7,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: const [
+                Text(
+                  "NCT 2020",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 7,
+          ),
+          Container(
+            decoration: BoxDecoration(
+                border: Border.all(
+                    color: Colors.green,
+                    width: 2,
+                    style: BorderStyle.solid)),
+            width: 300,
+            height: 150,
+            child: Row(
+              children: [
+                Container(
+                  padding: EdgeInsets.only(left: 10),
+                  child: Image.network(
+                    "https://imgx.sonora.id/crop/0x0:0x0/700x465/photo/2020/04/14/256609166.png",
+                  ),
+                  decoration: BoxDecoration(),
+                  height: 180,
+                  width: 210,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Container(
+                  width: 180,
+                  height: 140,
+                  child: Center(
+                      child: Text(
+                    "NCT DREAM",
+                    style: TextStyle(fontSize: 15),
+                  )),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+                border: Border.all(
+                    color: Colors.green,
+                    width: 2,
+                    style: BorderStyle.solid)),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: const [
+                Text(
+                  "2031710084",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+                border: Border.all(
+                    color: Colors.green,
+                    width: 2,
+                    style: BorderStyle.solid)),
+            width: 300,
+            height: 150,
+            child: Row(
+              children: [
+                Container(
+                  padding: EdgeInsets.only(left: 10),
+                  child: Image.network(
+                    "https://www.wowkeren.com/images/news/00200224.jpg",
+                  ),
+                  decoration: const BoxDecoration(),
+                  width: 210,
+                  height: 210,
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Container(
+                  width: 180,
+                  height: 140,
+                  child: Center(
+                      child: Text(
+                    "NCT 127",
+                    style: TextStyle(fontSize: 15),
+                  )),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+                border: Border.all(
+                    color: Colors.green,
+                    width: 2,
+                    style: BorderStyle.solid)),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: const [
+                Text(
+                  "Dea Vernanda R.N",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ]),
       ),
-    );
-  }
-}
-
-class konvertButton extends StatelessWidget {
-  konvertButton({
-    Key? key,
-    required this.text1,
-    required this.konversi,
-  }) : super(key: key);
-
-  final TextEditingController text1;
-  final Function konversi;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 50,
-      width: double.infinity,
-      child: ElevatedButton(
-        onPressed: () {
-          konversi();
-          text1.clear();
-        },
-        child: Text(
-          "Konversi",
-          style: TextStyle(fontSize: 30),
-        ),
-      ),
-    );
-  }
-}
-
-class inputSuhu extends StatelessWidget {
-  const inputSuhu({
-    Key? key,
-    required this.text1,
-  }) : super(key: key);
-
-  final TextEditingController text1;
-
-  @override
-  Widget build(BuildContext context) {
-    return TextField(
-        controller: text1,
-        decoration: InputDecoration(hintText: "Masukkan Suhu"),
-        keyboardType: TextInputType.number,
-        inputFormatters: <TextInputFormatter>[
-          FilteringTextInputFormatter.digitsOnly
-        ]);
-  }
-}
-
-class Hasil extends StatelessWidget {
-  const Hasil({
-    Key? key,
-    required this.kelvin,
-    required this.reamor,
-  }) : super(key: key);
-
-  final double kelvin;
-  final double reamor;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        Column(
-          children: [
-            Text(
-              "Suhu Dalam Kelvin",
-              style: TextStyle(fontSize: 30),
-            ),
-            Text(
-              "$kelvin",
-              style: TextStyle(fontSize: 30),
-            ),
-          ],
-        ),
-        Column(
-          children: [
-            Text(
-              "Suhu Dalam Reamor",
-              style: TextStyle(fontSize: 30),
-            ),
-            Text(
-              "$reamor",
-              style: TextStyle(fontSize: 30),
-            ),
-          ],
-        ),
-      ],
-    );
+    ));
   }
 }
